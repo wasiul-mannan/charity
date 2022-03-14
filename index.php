a480ab701042c7e57dd6c30b838815ba2e665d18
sabrina sultana <sameera.cse.tec@gmail.com>
2022-03-14 19:58:26 +0600

    index error fixed

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 include("includes/db.php");session_start();
-if (!isset($_SESSION['admin_email'])) {
-    echo "<script> window.open('login.php','_self')</script>";
-}
+
 ?>
 
 <!DOCTYPE html>
